e61c77cf3d954f975a5930bc00eaa29e093f579d
Minkyung-DouKevinMom <lovecs82@gmail.com>
2026-03-25 14:53:57 +0900

    6_분석.py 업데이트

--- pages/6_분석.py
+++ pages/6_분석.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-from modules.analytics.export_price_view import render as render_export_price_view
 from modules.analytics.price_analysis import render as render_price_analysis
 
 st.set_page_config(page_title="분석", layout="wide")
@@ -9,11 +8,9 @@ st.title("분석")
 
 menu = st.radio(
     "메뉴 선택",
-    ["수출가격 조회", "가격분석 통합조회"],
+    ["가격분석 통합조회"],
     horizontal=True
 )
 
-if menu == "수출가격 조회":
-    render_export_price_view()
-elif menu == "가격분석 통합조회":
+if menu == "가격분석 통합조회":
     render_price_analysis()
