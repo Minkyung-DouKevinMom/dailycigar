5f0d3f7d2af44ae4c261fce7adeb6f82fd028dda
Minkyung-DouKevinMom <lovecs82@gmail.com>
2026-03-25 15:07:41 +0900

    6_분석.py 업데이트

--- pages/6_분석.py
+++ pages/6_분석.py
@@ -1,16 +1,24 @@
 import streamlit as st
 
-from modules.analytics.price_analysis import render as render_price_analysis
+from modules.analytics.price_analysis_view import render as render_price
+from modules.analytics.period_compare_view import render as render_period
+from modules.analytics.partner_analysis_view import render as render_partner
+from modules.analytics.brand_analysis_view import render as render_brand
 
 st.set_page_config(page_title="분석", layout="wide")
-
 st.title("분석")
 
 menu = st.radio(
     "메뉴 선택",
-    ["가격분석 통합조회"],
-    horizontal=True
+    ["가격 통합분석", "기간 비교", "거래처 분석", "브랜드 분석"],
+    horizontal=True,
 )
 
-if menu == "가격분석 통합조회":
-    render_price_analysis()
+if menu == "가격 통합분석":
+    render_price()
+elif menu == "기간 비교":
+    render_period()
+elif menu == "거래처 분석":
+    render_partner()
+elif menu == "브랜드 분석":
+    render_brand()
